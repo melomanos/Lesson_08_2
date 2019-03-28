40e49c1acc1fa80336b683ba039360a41bbe1c39
melomanos <philippov_mihail@mail.ru>
2019-03-28 15:18:33 +0300

    Lesson_05_2

--- app/src/main/java/com/example/androidfilippov/CalcActivity.java
+++ app/src/main/java/com/example/androidfilippov/CalcActivity.java
@@ -3,12 +3,10 @@ package com.example.androidfilippov;
 import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class CalcActivity extends AppCompatActivity {
 
@@ -32,13 +30,37 @@ public class CalcActivity extends AppCompatActivity {
     private Button plusMinusBtn;
     private Button percentBtn;
     private Button equalsBtn;
+
     private Button changeMode;
     private Button changeModeI;
     private RelativeLayout usualMode;
     private RelativeLayout engineeringMode;
 
+    private TextView resultTViewCalcI;
+    private Button zeroBtnI;
+    private Button oneBtnI;
+    private Button twoBtnI;
+    private Button threeBtnI;
+    private Button fourBtnI;
+    private Button fiveBtnI;
+    private Button sixBtnI;
+    private Button sevenBtnI;
+    private Button eightBtnI;
+    private Button nineBtnI;
+    private Button pointBtnI;
+    private Button cancelBtnI;
+    private Button plusBtnI;
+    private Button minusBtnI;
+    private Button multiplyBtnI;
+    private Button divideBtnI;
+    private Button plusMinusBtnI;
+    private Button percentBtnI;
+    private Button equalsBtnI;
+
     private int operand1;
     private int operand2;
+    private int operand3;
+    private int operand4;
     private int flagAction = 0;
     private int preResult = 0;
     private int preChange;
@@ -143,7 +165,7 @@ public class CalcActivity extends AppCompatActivity {
         cancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                resultTViewCalc.setText("0");
+                resultTViewCalc.setText("");
             }
         });
 
@@ -235,6 +257,174 @@ public class CalcActivity extends AppCompatActivity {
                 engineeringMode.setVisibility(View.VISIBLE);
             }
         });
+
+        zeroBtnI.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                resultTViewCalcI.setText(resultTViewCalcI.getText() + "0");
+            }
+        });
+
+        oneBtnI.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                resultTViewCalcI.setText(resultTViewCalcI.getText() + "1");
+            }
+        });
+
+        twoBtnI.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                resultTViewCalcI.setText(resultTViewCalcI.getText() + "2");
+            }
+        });
+
+        threeBtnI.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                resultTViewCalcI.setText(resultTViewCalcI.getText() + "3");
+            }
+        });
+
+        fourBtnI.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                resultTViewCalcI.setText(resultTViewCalcI.getText() + "4");
+            }
+        });
+
+        fiveBtnI.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                resultTViewCalcI.setText(resultTViewCalcI.getText() + "5");
+            }
+        });
+
+        sixBtnI.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                resultTViewCalcI.setText(resultTViewCalcI.getText() + "6");
+            }
+        });
+
+        sevenBtnI.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                resultTViewCalcI.setText(resultTViewCalcI.getText() + "7");
+            }
+        });
+
+        eightBtnI.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                resultTViewCalcI.setText(resultTViewCalcI.getText() + "8");
+            }
+        });
+
+        nineBtnI.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                resultTViewCalcI.setText(resultTViewCalcI.getText() + "9");
+            }
+        });
+
+        pointBtnI.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                resultTViewCalcI.setText(resultTViewCalcI.getText() + ".");
+            }
+        });
+
+        cancelBtnI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resultTViewCalcI.setText("");
+            }
+        });
+
+        plusBtnI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flagAction = 1;
+                operand3 = Integer.parseInt((String) resultTViewCalcI.getText());
+                resultTViewCalcI.setText("");
+            }
+        });
+
+        minusBtnI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flagAction = 2;
+                operand3 = Integer.parseInt((String) resultTViewCalcI.getText());
+                resultTViewCalcI.setText("");
+            }
+        });
+
+        multiplyBtnI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flagAction = 3;
+                operand3 = Integer.parseInt((String) resultTViewCalcI.getText());
+                resultTViewCalcI.setText("");
+            }
+        });
+
+        divideBtnI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flagAction = 4;
+                operand3 = Integer.parseInt((String) resultTViewCalcI.getText());
+                resultTViewCalcI.setText("");
+            }
+        });
+
+        plusMinusBtnI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt((String) resultTViewCalcI.getText());
+                preChange = a - a - a;
+                String change = String.valueOf(preChange);
+                resultTViewCalcI.setText(change);
+            }
+        });
+
+        equalsBtnI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operand4 = Integer.parseInt((String) resultTViewCalcI.getText());
+
+                switch(flagAction) {
+                    case 1:
+                        preResult = operand3 + operand4;
+                        break;
+                    case 2:
+                        preResult = operand3 - operand4;
+                        break;
+                    case 3:
+                        preResult = operand3 * operand4;
+                        break;
+                    case 4:
+                        preResult = operand3 / operand4;
+                        break;
+                    default:
+                        resultTViewCalcI.setText("Операция не задана");
+                }
+
+                String result = String.valueOf(preResult);
+                resultTViewCalcI.setText(result);
+            }
+        });
     }
 
     private void initViews() {
@@ -258,9 +448,31 @@ public class CalcActivity extends AppCompatActivity {
         plusMinusBtn = findViewById(R.id.plusMinusBtn);
         percentBtn = findViewById(R.id.percent);
         equalsBtn = findViewById(R.id.equalsBtn);
+
         changeMode = findViewById(R.id.changeMode);
         changeModeI = findViewById(R.id.changeModeI);
         usualMode = findViewById(R.id.usualMode);
         engineeringMode = findViewById(R.id.engineeringMode);
+
+        resultTViewCalcI = findViewById(R.id.resultTViewCalcI);
+        zeroBtnI = findViewById(R.id.zeroBtnI);
+        oneBtnI = findViewById(R.id.oneBtnI);
+        twoBtnI = findViewById(R.id.twoBtnI);
+        threeBtnI = findViewById(R.id.threeBtnI);
+        fourBtnI = findViewById(R.id.fourBtnI);
+        fiveBtnI = findViewById(R.id.fiveBtnI);
+        sixBtnI = findViewById(R.id.sixBtnI);
+        sevenBtnI = findViewById(R.id.sevenBtnI);
+        eightBtnI = findViewById(R.id.eightBtnI);
+        nineBtnI = findViewById(R.id.nineBtnI);
+        pointBtnI = findViewById(R.id.pointBtnI);
+        cancelBtnI = findViewById(R.id.cancelBtnI);
+        plusBtnI = findViewById(R.id.plusBtnI);
+        minusBtnI = findViewById(R.id.minusBtnI);
+        multiplyBtnI = findViewById(R.id.multiplyBtnI);
+        divideBtnI = findViewById(R.id.divideBtnI);
+        plusMinusBtnI = findViewById(R.id.plusMinusBtnI);
+        percentBtnI = findViewById(R.id.percentBtnI);
+        equalsBtnI = findViewById(R.id.equalsBtnI);
     }
 }
